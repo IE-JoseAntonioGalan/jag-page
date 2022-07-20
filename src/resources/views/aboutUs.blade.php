30442dc9f23abd963c2a7e195cf4f98d7cf6a2a8
Willy <iejoseantoniogalan1@gmail.com>
2022-07-19 20:07:15 -0500

    Cambios nuevos

--- src/resources/views/aboutUs.blade.php
+++ src/resources/views/aboutUs.blade.php
@@ -57,6 +57,61 @@
 
     <!-- about section starts  -->
 
+    <section class="about">
+
+        <div class="content">
+            <p>
+Coro <br>
+Siento orgullo gritar: ¡Soy galanista! <br>
+Defensor de las causas que nos unen, <br>
+siento orgullo gritar:¡Soy galanista! <br>
+El Galán es mi luz, ¡viva su lumbre! <br><br>
+
+Canto al sol que se asoma en las mañanas, <br>
+al crisol de la ciencia y sus linderos, <br>
+al colegio que evoca las campañas <br>
+de Galán, el valiente comunero. <br><br>
+
+Eres flor, juventud, algarabía <br>
+cantera de valores y costumbres, <br>
+fuego intenso, virtud, autonomía, <br>
+caminos que nos llevan a la cumbre. <br><br>
+
+(Coro) <br>
+Siento orgullo gritar: ¡Soy galanista! <br>
+Defensor de las causas que nos unen, <br>
+siento orgullo gritar:¡Soy galanista! <br>
+El Galán es mi luz, ¡viva su lumbre! <br><br>
+
+Cumaral es el cielo en que destellas <br>
+verdad, amor, alegre vestidura, <br>
+deportistas que suman mil estrellas, <br>
+lunadas y derroches de cultura. <br><br>
+
+En tus aulas se pulen sus linajes <br>
+que ostentan banderas coloridas, <br>
+defendemos el agua y el paisaje <br>
+origen y sustento de la vida. <br><br>
+
+(Coro) <br>
+Siento orgullo gritar: ¡Soy galanista! <br>
+Defensor de las causas que nos unen, <br>
+siento orgullo gritar:¡Soy galanista! <br>
+El Galán es mi luz, ¡viva su lumbre! <br><br>
+
+Autores: <br>
+Letra: Pedro Sánchez Ruíz <br>
+Música: Javier Manchego <br><br>
+
+            </p>
+
+<audio controls>
+    <source src="galanhimno.mp3">
+</audio>
+
+        </div>
+    </section>
+
     <!-- about section ends -->
 
 @endsection
